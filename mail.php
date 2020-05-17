ae25490fbf01bdf33aac66c8e681248d06a9b876
Adam Franks <52870365+adfranks@users.noreply.github.com>
2020-05-17 17:53:44 -0400

    tweaks for ios

--- mail.php
+++ mail.php
@@ -1,4 +1,3 @@
-// Back end for sending a message.
 <?php 
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -7,6 +6,13 @@ $formcontent = "From:" . $name . " Message:" . $comment;
 $recipient = "info@adfranks.com";
 $subject = "Web Development";
 $mailheader = "From:" . $email . "\r\n";
+
 mail($recipient, $subject, $formcontent, $mailheader);
-echo "Thank You!" . " <a href='portfolio.html'>Go back to the portfolio.</a>";
+
+echo "<div style='height:100%;text-align:center;background-color:#66ff66;color:green;padding:2em;font-size:3em;'>
+    <h1>Thank You!</h1>
+    <p><a href='index.html' style='color:#663300;'>
+    Portfolio</a><br><br>
+    <a href='https://github.com/adfranks' style='color:#663300;'>
+    GitHub</a></p></div>";
 ?>
